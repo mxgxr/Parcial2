970a4a8703515464f9bc882cf238a644a5c79b7d
mxgxr <manuela.gutierrezr@udea.edu.co>
2021-09-20 23:34:47 -0500

    Avance en qt de la funcion submuestreo

--- Parcial2-Informatica/main.cpp
+++ Parcial2-Informatica/main.cpp
@@ -1,20 +1,42 @@
 #include <iostream>
 #include <QImage>
 #include <vector>
+#include <iterator>
 
 using namespace std;
+
+vector<int> promedioColor(vector<vector<int>> &arr);
 
 int main()
 {
     string ruta;
-    int contador=0;
-    vector<vector<int>>matrizRGB;
+    int contador=0, altoProm, anchoProm, cont2=0;
+    vector<int>matrizRGB;
+    vector<vector<int>>matrizPixeles;
 
     cout << "Ingrese la ruta de la imagen a cargar: " << endl;
     cin >> ruta;
 
     QImage imagen(ruta.c_str());
 
+    anchoProm=imagen.width()/12;
+    altoProm=imagen.height()/12;
+
+
+    while(contador!=imagen.width()*imagen.height()){
+        for(int x=contador; x<anchoProm; x++){
+            for(int y=cont2; y<altoProm; y++){
+                matrizRGB[0]=imagen.pixelColor(x,y).red();
+                matrizRGB[1]=imagen.pixelColor(x,y).green();
+                matrizRGB[2]=imagen.pixelColor(x,y).blue();
+                matrizPixeles.push_back(matrizRGB);
+                cont2++;
+            }
+            contador++;
+            cont2=0;
+        }
+    }
+/*
     for(int x=0; x<imagen.width(); x++){
         for(int y=0; y<imagen.height(); y++){
             matrizRGB[contador].push_back(imagen.pixelColor(x,y).red());
@@ -22,8 +44,30 @@ int main()
             matrizRGB[contador].push_back(imagen.pixelColor(x,y).blue());
             contador++;
         }
-    }
+    }*/
 
 
     return 0;
+}
+
+vector<int> promedioColor(vector<vector<int>> &arr)
+{
+    //vector<vector<int>>::iterator k;
+    //vector<int>::iterator j;
+    vector<int>promRGB;
+    int rojo=0, verde=0, azul=0;
+    for(unsigned int k=0; k<arr.size(); k++){
+        rojo+=arr[k][0];
+        verde+=arr[k][1];
+        azul+=arr[k][2];
+    }
+    rojo=rojo/arr.size();
+    verde=verde/arr.size();
+    azul=azul/arr.size();
+
+    promRGB[0]=rojo;
+    promRGB[1]=verde;
+    promRGB[2]=azul;
+
+    return promRGB;
 }
